24a723f15cc696e15e58fbdc3cd42daf3ef082fd
naveens007 <naveens007@gmail.com>
2016-06-19 16:17:08 +0530

    * Exposed the API properly.

--- module4-solution/easier/SpeakGoodBye.js
+++ module4-solution/easier/SpeakGoodBye.js
@@ -1,11 +1,12 @@
 // NOTE! The steps in this file are basically identical to the ones you
 // performed in the SpeakHello.js file.
 
+
+(function (window){
+
 // STEP 6: Wrap the entire contents of SpeakGoodBye.js inside of an IIFE
 // See Lecture 52, part 2
 var byeSpeaker = {};
-
-(function (){
 
 // STEP 7: Create an object, called 'byeSpeaker' to which you will attach
 // the "speak" method and which you will expose to the global context
@@ -22,8 +23,10 @@ byeSpeaker.speak = function (name) {
   console.log(speakWord + " " + name);
 }
 
-}) (); // The IFFE
-
 // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
 // 'byeSpeaker' on the global scope as well.
 // xxxx.xxxx = byeSpeaker;
+window.byeSpeaker = byeSpeaker;
+
+}) (window); // The IFFE
+
--- module4-solution/easier/SpeakHello.js
+++ module4-solution/easier/SpeakHello.js
@@ -2,12 +2,12 @@
 // See Lecture 52, part 2
 
 
+(function (window) {
 // STEP 3: Create an object, called 'helloSpeaker' to which you will attach
 // the "speak" method and which you will expose to the global context
 // See Lecture 52, part 1
-var helloSpeaker = {};
+	var helloSpeaker = {};
 // DO NOT attach the speakWord variable to the 'helloSpeaker' object.
-(function () {
 
 	var speakWord = "Hello";
 
@@ -17,11 +17,14 @@ var helloSpeaker = {};
 	helloSpeaker.speak = function (name) {
   		console.log(speakWord + " " + name);
  	}
-}) (); // STEP 2 Done: Made an IIFE
+ 	// STEP 5: Expose the 'helloSpeaker' object to the global scope. Name it
+	// 'helloSpeaker' on the global scope as well.
+	// See Lecture 52, part 2
+	// (Note, Step 6 will be done in the SpeakGoodBye.js file.)
+	// xxxx.xxxx = helloSpeaker;
+
+ 	window.helloSpeaker = helloSpeaker;
+
+}) (window); // STEP 2 Done: Made an IIFE
 
 
-// STEP 5: Expose the 'helloSpeaker' object to the global scope. Name it
-// 'helloSpeaker' on the global scope as well.
-// See Lecture 52, part 2
-// (Note, Step 6 will be done in the SpeakGoodBye.js file.)
-// xxxx.xxxx = helloSpeaker;
